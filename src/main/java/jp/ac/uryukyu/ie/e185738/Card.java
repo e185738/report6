9fe85c3096ffddc88e31290389fa03e8e5df539d
Rei Yamashiro <e185738@ie.u-ryukyu.ac.jp>
2019-01-21 14:42:40 +0900

    player commit

--- src/main/java/jp/ac/uryukyu/ie/e185738/Card.java
+++ src/main/java/jp/ac/uryukyu/ie/e185738/Card.java
@@ -2,23 +2,28 @@ package jp.ac.uryukyu.ie.e185738;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * カードクラス。
+ * String[] suit; //カードの記号の配列
+ * int[] num; //カードの数字の元となる数字の配列。
+ * String type; //１〜１３のカードの数字。
+ * String cards; //記号と数字を含むカード。
+ */
 public class Card {
-    private String[] suit;
-    private int[] num;
-    private String type;
-    private String cards;
+    String[] suit;
+    int[] num;
+    String type;
+    String cards;
 
     public Card() {
         this.suit = new String[]{"ハート", "クラブ", "スペード", "ダイヤ", "ジョーカー"};
         this.num = new int[54];
-
     }
     public ArrayList cardset() {
         ArrayList<String> list = new ArrayList<String>();
         for (int i = 1; i <= 54; i++) {
             num[i - 1] = i - 1;
         }
-
         for (int j = 1; j <= 54; j++) {
             this.type = suit[num[j - 1] / 13];
             this.cards = type + "の" + (num[j - 1] % 13 + 1);
@@ -27,7 +32,6 @@ public class Card {
         return list;
     }
     }
-
 
 
 
--- src/main/java/jp/ac/uryukyu/ie/e185738/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e185738/Main.java
@@ -1,5 +1,6 @@
 package jp.ac.uryukyu.ie.e185738;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] Args){
@@ -8,7 +9,14 @@ public class Main {
         oldMaid.shuflle();
         System.out.println();
         Player player = new Player();
+
         player.play();
+        int input;
+        Scanner in = new Scanner(System.in);
+        System.out.println("何人で遊びますか？");
+        input = in.nextInt();
+
+        player.playerset(input);
 
 
     }
